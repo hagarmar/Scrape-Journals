2ffb7daa930be2777d37ce52619cb2009ac9b31d
Hagar Mar <hagar.mar@gmail.com>
2015-06-02 19:41:55 -0400

    More data
    Getting more article data from the site: author names, article name,
    type, date

--- scrape_test.py
+++ scrape_test.py
@@ -1,12 +1,18 @@
 from bs4 import BeautifulSoup
 import requests 
 import re
+import pymongo
+from pyongo import MongoClient
+import time
+import datetime
 
 # consts
 ARTICLE_TYPES = ['ResearchArticles', 'Reports'] # which authors to look for
 SCIENCE_MAIN_PAGE = "http://www.sciencemag.org/"
 VOLUME_NOTATION = "Vol. "
 ISSUE_NOTATION = "#"
+MONTHS = {"January":1, "February":2, "March":3, "April": 4, "May":5, "June":6, "July":7, 
+		  "August":8, "September":9, "October":10, "November":11, "December":12}
 
 # returns soup object for website_url or None if doesn't work
 def get_soup_webpage(website_url):
@@ -51,13 +57,58 @@ def get_authors_Science(website_url, is_all_authors=True):
 
 	return authors 
 
+def get_all_data_issue_view(website_url):
+	soup = get_soup_webpage(website_url)
+	if not soup:
+		return None
+
+	all_articles_in_view = []
+	
+	# for our article_types return what we need:
+	for article_type in ARTICLE_TYPES:
+
+		# list with all articles of specific type in this issue
+		all_articles_of_type = soup.select("div.pub-section-%s div.cit-metadata", article_type)
+
+		for article in all_articles_of_type:
+
+			# get article title
+			title_raw=article.select("h4")
+			p=re.compile('>.*?<') # get all elements of the title, between > and <
+			title_matches = p.findall(str(title_raw))
+
+			full_title = []
+			for part in title_matches:
+				full_title.append(part[1:-2]) # get without < and >
+
+			# get all article authors
+			all_article_authors = ([x.string for x in article.select("span.cit-auth-type-author")])
+		
+			# get author affiliations??
+
+			# get article date
+			article_date_raw = article.find("span", {"class":"cit-print-date"}).get_text().replace(":", "").strip().split()
+			date_format = datetime.date(int(article_date_raw[2]), MONTHS[article_date_raw[1]], int(article_date_raw[0]))
+
+			# get journal name
+			journal_name = article.find("abbr").string
+
+			# put all in list of dicts
+			all_articles_in_view.append({"Title":full_title, 
+										 "Type":article_type, 
+										 "Authors": all_article_authors, 
+										 "Date":date_format,
+										 "Journal":journal_name})
+
+	return all_articles_in_view
+
 # scrape author names from n_issues
 # returns list with all author names
-def scrape_Science(n_of_issues):
+def scrape_Science_simple(n_of_issues):
 
 	# get curr issue and vol from main page
 	vol_number, issue_number = get_latest_issue_Science()
-	all_issue_authors = []
+	all_issue_authors = []	
 
 	while n_of_issues>0
 		
@@ -71,7 +122,40 @@ def scrape_Science(n_of_issues):
 			all_issue_authors.append(get_authors_Science(website_url, True))
 			issue_number = issue_number - 1
 			n_of_issues = n_of_issues - 1
+			time.sleep(1) # pause for 1 sec between pages
+
 	return all_issue_authors
 
+# scrape article data from science journal
+# returns list of dicts from each issue and enter to MongoDB
+def scrape_Science_db(n_of_issues):
+
+	# connect to mongoDB
+	connection = MongoClient('localhost', 27017)
+	db = connection.journals.authors
+
+	# get curr issue and vol from main page
+	vol_number, issue_number = get_latest_issue_Science()
+	all_issue_authors = []	
+
+	while n_of_issues>0
+		
+		website_url = "http://www.sciencemag.org/content/" + str(vol_number) + "/" + str(issue_number) + ".toc"
+		response = get_authors_Science(website_url, True)
+		
+		if not response:
+			vol_number = vol_number - 1
+			continue
+		else:
+			all_issue_authors.append(get_authors_Science(website_url, True))
+			issue_number = issue_number - 1
+			n_of_issues = n_of_issues - 1
+			time.sleep(1) # pause for 1 sec between pages
+
+	return all_issue_authors
+
+	connection.close()
+
+
 #print get_authors_Science("http://www.sciencemag.org/content/346/6215.toc", True)
-print get_latest_issue_Science()
+#print get_latest_issue_Science()

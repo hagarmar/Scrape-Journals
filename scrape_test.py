f080d78e21e2bb62c4a048a9838aa931d570f94c
Hagar Mar <hagar.mar@gmail.com>
2015-06-10 17:53:38 -0400

    After several tests
    1. need to check if several volumes
    2. need to check if vol+issue already in db

--- scrape_test.py
+++ scrape_test.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests 
 import re
 import pymongo
-from pyongo import MongoClient
+from pymongo import MongoClient
 import time
 import datetime
 
@@ -35,7 +35,7 @@ def get_latest_issue_Science():
 	vol_number = current_issue_name.split(VOLUME_NOTATION)[1].split(',')[0]
 	issue_number = current_issue_name.split(ISSUE_NOTATION)[1].split('<')[0]
 
-	return vol_number, issue_number
+	return int(vol_number), int(issue_number)
 
 # returns list of authors from a website_url in Science magazine, certain issue + vol
 # if is_all_authors=True, returns all authors
@@ -68,7 +68,7 @@ def get_all_data_issue_view(website_url):
 	for article_type in ARTICLE_TYPES:
 
 		# list with all articles of specific type in this issue
-		all_articles_of_type = soup.select("div.pub-section-%s div.cit-metadata", article_type)
+		all_articles_of_type = soup.select("div.pub-section-%s div.cit-metadata" % article_type)
 
 		for article in all_articles_of_type:
 
@@ -88,17 +88,17 @@ def get_all_data_issue_view(website_url):
 
 			# get article date
 			article_date_raw = article.find("span", {"class":"cit-print-date"}).get_text().replace(":", "").strip().split()
-			date_format = datetime.date(int(article_date_raw[2]), MONTHS[article_date_raw[1]], int(article_date_raw[0]))
+			date_format = datetime.datetime(int(article_date_raw[2]), MONTHS[article_date_raw[1]], int(article_date_raw[0]))
 
 			# get journal name
 			journal_name = article.find("abbr").string
 
 			# put all in list of dicts
-			all_articles_in_view.append({"Title":full_title, 
-										 "Type":article_type, 
+			all_articles_in_view.append({"Title": str(full_title), 
+										 "Type": article_type, 
 										 "Authors": all_article_authors, 
-										 "Date":date_format,
-										 "Journal":journal_name})
+										 "Date": date_format,
+										 "Journal": journal_name})
 
 	return all_articles_in_view
 
@@ -110,11 +110,11 @@ def scrape_Science_simple(n_of_issues):
 	vol_number, issue_number = get_latest_issue_Science()
 	all_issue_authors = []	
 
-	while n_of_issues>0
+	while n_of_issues>0:
 		
-		website_url = "http://www.sciencemag.org/content/" + str(vol_number) + "/" + str(issue_number) + ".toc"
+		website_url = SCIENCE_MAIN_PAGE + "content/" + str(vol_number) + "/" + str(issue_number) + ".toc"
 		response = get_authors_Science(website_url, True)
-		
+
 		if not response:
 			vol_number = vol_number - 1
 			continue
@@ -132,29 +132,31 @@ def scrape_Science_db(n_of_issues):
 
 	# connect to mongoDB
 	connection = MongoClient('localhost', 27017)
-	db = connection.journals.authors
+	db = connection.journals
+	collection = db.authors
 
 	# get curr issue and vol from main page
 	vol_number, issue_number = get_latest_issue_Science()
-	all_issue_authors = []	
 
-	while n_of_issues>0
+	while n_of_issues>0:
 		
-		website_url = "http://www.sciencemag.org/content/" + str(vol_number) + "/" + str(issue_number) + ".toc"
-		response = get_authors_Science(website_url, True)
-		
+		website_url = SCIENCE_MAIN_PAGE + "content/" + str(vol_number) + "/" + str(issue_number) + ".toc"
+		response = get_all_data_issue_view(website_url)
+
 		if not response:
 			vol_number = vol_number - 1
 			continue
 		else:
-			all_issue_authors.append(get_authors_Science(website_url, True))
+
+			# put data in db
+			collection.insert(response)
 			issue_number = issue_number - 1
 			n_of_issues = n_of_issues - 1
 			time.sleep(1) # pause for 1 sec between pages
 
-	return all_issue_authors
-
 	connection.close()
+
+	return None
 
 
 #print get_authors_Science("http://www.sciencemag.org/content/346/6215.toc", True)
